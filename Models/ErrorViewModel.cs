d9bace4218e092a70344ab3a7a100c3d9d0c9ecc
Ethan Scott Parsons <ST10299399@vcconnect.edu.za>
2025-05-09 15:50:21 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10299399_PROG7311_GreenEnergy_POE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
