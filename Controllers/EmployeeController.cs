7230ca3160c3065eaee7dd1bcc8db1abc6a8bc88
Ethan Scott Parsons <ST10299399@vcconnect.edu.za>
2025-05-10 14:19:46 +0200

    Functionality for employees to add farmers

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ST10299399_PROG7311_GreenEnergy_POE.Models;   
+using Microsoft.AspNetCore.Mvc;
 
 namespace ST10299399_PROG7311_GreenEnergy_POE.Controllers
 {
     public class EmployeeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        
+        public EmployeeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult AddFarmer()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddFarmer(Farmer farmer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Farmers.Add(farmer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(farmer);
         }
     }
 }
