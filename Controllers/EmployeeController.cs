f5b83c2bf293e96a764d00523e04920460c21177
Ethan Scott Parsons <ST10299399@vcconnect.edu.za>
2025-05-10 14:12:53 +0200

    Added farmer and employee controllers to handle functionality

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ST10299399_PROG7311_GreenEnergy_POE.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
