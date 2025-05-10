ac63017e9e01f5202bdcd5b5981fff0c3ba7cca2
Ethan Scott Parsons <ST10299399@vcconnect.edu.za>
2025-05-10 15:49:05 +0200

    Change ProductDb to use the stuff required

--- Models/Product.cs
+++ Models/Product.cs
@@ -6,8 +6,8 @@ namespace ST10299399_PROG7311_GreenEnergy_POE.Models
     {
        public int ProductId { get; set; }
         public string ProductName { get; set; }
-        public string ProductDescription { get; set; }
         public double ProductPrice { get; set; }
+        public DateTime ProductDate { get; set; }
 
         public int FarmerId { get; set; }
         public Farmer Farmer { get; set; }
