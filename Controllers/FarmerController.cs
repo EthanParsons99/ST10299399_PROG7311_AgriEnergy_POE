f5e04d6eed2f5c21d579d5abc6e48e8b8703cf93
Ethan Scott Parsons <ST10299399@vcconnect.edu.za>
2025-05-10 14:17:21 +0200

    Added functionality to add product as a farmer

--- Controllers/FarmerController.cs
+++ Controllers/FarmerController.cs
@@ -1,12 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ST10299399_PROG7311_GreenEnergy_POE.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ST10299399_PROG7311_GreenEnergy_POE.Controllers
 {
     public class FarmerController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public FarmerController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var farmers = await _context.Farmers.Include(f => f.Products).ToListAsync();
+            return View(farmers);
+        }
+
+        public IActionResult AddProduct(int farmerId)
+        {
+            var model = new Product
+            {
+                FarmerId = farmerId
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddProduct(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
         }
     }
 }
