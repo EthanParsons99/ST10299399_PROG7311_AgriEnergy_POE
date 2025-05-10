a37e10a5c9e95570cf40675909e57e460d939bba
Ethan Scott Parsons <ST10299399@vcconnect.edu.za>
2025-05-10 15:57:51 +0200

    Updated farmer DB

---
+++ Migrations/20250510135521_DatabaseUpdateFarmer.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ST10299399_PROG7311_GreenEnergy_POE.Migrations
+{
+    /// <inheritdoc />
+    public partial class DatabaseUpdateFarmer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FarmerEmail",
+                table: "Farmers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FarmerPhone",
+                table: "Farmers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FarmerSurname",
+                table: "Farmers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FarmerEmail",
+                table: "Farmers");
+
+            migrationBuilder.DropColumn(
+                name: "FarmerPhone",
+                table: "Farmers");
+
+            migrationBuilder.DropColumn(
+                name: "FarmerSurname",
+                table: "Farmers");
+        }
+    }
+}
--- Models/Farmer.cs
+++ Models/Farmer.cs
@@ -6,6 +6,11 @@ namespace ST10299399_PROG7311_GreenEnergy_POE.Models
     {
         public int FarmerId { get; set; }
         public string FarmerName { get; set; }
+
+        public string FarmerSurname { get; set; }
+
+        public string FarmerPhone { get; set; }
+        public string FarmerEmail { get; set; }
         public string FarmerPassword { get; set; }
 
         public ICollection<Product> Products { get; set; }
