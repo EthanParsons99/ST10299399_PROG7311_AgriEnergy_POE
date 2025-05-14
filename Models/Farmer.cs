010d7123014f2f34d510d231920f3e496443bf35
Ethan Scott Parsons <ST10299399@vcconnect.edu.za>
2025-05-14 14:29:52 +0200

    Farmer Model comments

--- Models/Farmer.cs
+++ Models/Farmer.cs
@@ -10,22 +10,19 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System;
 using System.Text.Json.Serialization;
 
+// Farmer class representing a farmer in the system
+// It contains properties for FarmerId, FarmerName, FarmerSurname, FarmerPhone, FarmerEmail, and FarmerPassword
 namespace ST10299399_PROG7311_GreenEnergy_POE.Models
 {
     public class Farmer
     {
         public int FarmerId { get; set; }
         public string FarmerName { get; set; }
-
         public string FarmerSurname { get; set; }
-
         public string FarmerPhone { get; set; }
         public string FarmerEmail { get; set; }
         public string FarmerPassword { get; set; }
-
-    
         public ICollection<Product>? Products { get; set; }
-
     }
 }
  //-----------================End of file=================--------------//
